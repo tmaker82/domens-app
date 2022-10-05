4ce629fa80d92bd83027fdb395017e032cf2dd10
syakushev <syakushev@diasoft.ru>
2022-10-05 10:03:13 +0300

    Добавление punycode конвертера

--- perle/src/App.js
+++ perle/src/App.js
@@ -5,8 +5,11 @@ import "./App.css";
 import MoneyComponent from "./Components/Money/MoneyComponent";
 import FooterComponent from "./Components/Footer/FooterComponent";
 import HeaderComponent from "./Components/Header/HeaderComponent";
+const punycode = require("punycode");
 
 function App() {
+  console.log(punycode.toUnicode("xn--e1aarog.xn--p1ai"));
+
   return (
     <div className="App">
       <HeaderComponent></HeaderComponent>
